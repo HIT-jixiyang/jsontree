bdcfbcc1f83859e236f6d7d85530db5bf019c875
lyMeiSEU <213173625@seu.edu.cn>
2018-12-17 21:10:03 +0800

    post wrong

--- src/main/java/jsontree/JsonTree.java
+++ src/main/java/jsontree/JsonTree.java
@@ -1,9 +1,21 @@
 package jsontree;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.activation.MimetypesFileTypeMap;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -69,6 +81,96 @@ public class JsonTree {
 		return jsonStr;
 	}
 
+	public static String formUpload(String urlStr, Map<String, String> fileMap) {
+		String res = "";
+		HttpURLConnection conn = null;
+		String BOUNDARY = "---------------------------123821742118716"; // boundary就是request头和上传文件内容的分隔符
+		try {
+			URL url = new URL(urlStr);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(5000);
+			conn.setReadTimeout(30000);
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setUseCaches(false);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Connection", "Keep-Alive");
+			conn.setRequestProperty("User-Agent",
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763");
+			conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+			conn.setRequestProperty("Origin", "http://shibietu.wwei.cn");
+			conn.setRequestProperty("Host", "shibietu.wwei.cn");
+			conn.setRequestProperty("Referer", "http://shibietu.wwei.cn/zhiwu.html");
+			OutputStream out = new DataOutputStream(conn.getOutputStream());
+
+			// file
+			if (fileMap != null) {
+				Iterator iter = fileMap.entrySet().iterator();
+				while (iter.hasNext()) {
+					Map.Entry entry = (Map.Entry) iter.next();
+					String inputName = (String) entry.getKey();
+					String inputValue = (String) entry.getValue();
+					if (inputValue == null) {
+						continue;
+					}
+					File file = new File(inputValue);
+					String filename = file.getName();
+					String contentType = new MimetypesFileTypeMap().getContentType(file);
+					if (filename.endsWith(".png")) {
+						contentType = "image/png";
+					}
+					if (filename.endsWith(".jpg")) {
+						contentType = "image/jpg";
+					}
+					if (contentType == null || contentType.equals("")) {
+						contentType = "application/octet-stream";
+					}
+
+					StringBuffer strBuf = new StringBuffer();
+					// strBuf.append("\r\n").append("--").append(BOUNDARY).append("\r\n");
+					strBuf.append("\r\n").append("--").append(BOUNDARY).append("\r\n");
+					strBuf.append("Content-Disposition: form-data; name=\"" + inputName + "\"; filename=\"" + filename
+							+ "\"\r\n");
+					strBuf.append("Content-Type:" + contentType + "\r\n\r\n");
+					out.write(strBuf.toString().getBytes());
+
+					DataInputStream in = new DataInputStream(new FileInputStream(file));
+					int bytes = 0;
+					byte[] bufferOut = new byte[1024];
+					while ((bytes = in.read(bufferOut)) != -1) {
+						out.write(bufferOut, 0, bytes);
+					}
+					in.close();
+				}
+			}
+
+			// byte[] endData = ("\r\n--" + BOUNDARY + "--\r\n").getBytes();
+			// out.write(endData);
+			out.flush();
+			out.close();
+
+			// 读取返回数据
+			StringBuffer strBuf = new StringBuffer();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				strBuf.append(line).append("\n");
+			}
+			res = strBuf.toString();
+			reader.close();
+			reader = null;
+		} catch (Exception e) {
+			System.out.println("发送POST请求出错。" + urlStr);
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				conn.disconnect();
+				conn = null;
+			}
+		}
+		return res;
+	}
+
 	@SuppressWarnings("rawtypes")
 	public void analysisJson(Object objJson, String parent_type) {
 		JSONObject jsonObject = (JSONObject) objJson;
@@ -85,30 +187,23 @@ public class JsonTree {
 			} catch (Exception e) {
 				break;
 			}
-			
+
 		}
-		/*
-		 * // 如果obj为json数组 if (objJson instanceof JSONArray) { JSONArray objArray =
-		 * (JSONArray) objJson;
-		 * 
-		 * analysisJson(objArray.get(0), "Array");
-		 * 
-		 * } // 如果为json对象 else if (objJson instanceof JSONObject) { JSONObject
-		 * jsonObject = (JSONObject) objJson; Iterator it =
-		 * jsonObject.keySet().iterator(); while (it.hasNext()) { String key =
-		 * it.next().toString(); Object object = jsonObject.get(key); // 如果得到的是数组 if
-		 * (object instanceof JSONArray) { JSONArray objArray = (JSONArray) object;
-		 * analysisJson(objArray, "Array"); } // 如果key中是一个json对象 else if (object
-		 * instanceof JSONObject) { analysisJson((JSONObject) object, "Object"); } //
-		 * 如果key中是其他 else { ID++; System.out.println(ID + "------" + "[" + key + "]:" +
-		 * object.toString() + " " + parent_type); } } }
-		 */
 	}
 
 	public static void main(String[] args) {
-		JsonTree hw = new JsonTree();
-		JSONObject jsonObject = JSONObject.parseObject(hw.getJsonStr2());
-		hw.analysisJson(jsonObject, "Object");
+		String filepath = "C:\\Users\\83723\\Desktop\\IMGS\\IMG\\IMG1.jpg";
+		String urlStr = "http://shibietu.wwei.cn/fileupload.html?op=shibietu_zhiwu";
+		Map<String, String> textMap = null;
+		Map<String, String> fileMap = new HashMap<String, String>();
+		fileMap.put("file", filepath);
+		String ret = formUpload(urlStr,fileMap);
+		System.out.println(ret);
+		/*
+		 * JsonTree hw = new JsonTree(); JSONObject jsonObject =
+		 * JSONObject.parseObject(hw.getJsonStr2()); hw.analysisJson(jsonObject,
+		 * "Object");
+		 */
 
 	}
 }
